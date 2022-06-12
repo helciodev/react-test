4726093aef9f4fc8b95e40c4d1dd8358cfb20290
helciodev <helcioandre@live.com>
2022-06-12 10:17:48 +0100

    Create Header and Footer components

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+const FooterElement = styled.footer``;
+function Footer() {
+  return <Footer>I am footer</Footer>;
+}
+
+export default Footer;
---
+++ components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+const HeaderElement = styled.header``;
+function Header() {
+  return <Header>Header</Header>;
+}
+
+export default Header;
