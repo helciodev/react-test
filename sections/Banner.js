484ff33191df7ea79d69248e2158cf4d901293ce
helciodev <helcioandre@live.com>
2022-06-12 20:28:22 +0100

    Change asset name Add player image icon

--- sections/Banner.js
+++ sections/Banner.js
@@ -2,12 +2,16 @@ import Image from "next/image";
 import React from "react";
 import styled from "styled-components";
 import mobileCrypto from "../assets/mobile-crypto.png";
+import pattern from "../assets/pattern-background.png";
+import videoPlayer from "../assets/play-circle.png";
+
 const BannerSection = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
   padding: 2rem 3rem;
+
   .info {
     text-align: center;
     h2 {
@@ -28,10 +32,18 @@ const BannerSection = styled.section`
     justify-content: flex-end;
 
     img {
-      position: absolute;
+      /* position: absolute; */
+      top: 10px;
+      /* right: 0; */
     }
-
     .green-pattern-bg {
+      width: 200px;
+      height: 100px;
+      border: 1px solid red;
+      /* background-image: url(${pattern}); */
+      background: #236e57 url(${pattern});
+      border-top-left-radius: 100px;
+      border-bottom-left-radius: 100px;
     }
   }
 `;
@@ -54,6 +66,13 @@ function Banner() {
           alt='mobile phone illustrating crypto currency'
         />
         <div className='green-pattern-bg'></div>
+      </div>
+
+      <div className='call-to-action'>
+        <div className='video'>
+          <Image src={videoPlayer} alt='video player icon' />
+          <h3>Watch Video</h3>
+        </div>
       </div>
     </BannerSection>
   );
