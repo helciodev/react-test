b299fe5bfa868d9af5ad8cd4e2a02694a1c8ca8b
helciodev <helcioandre@live.com>
2022-06-12 23:33:30 +0100

    Create reusable card component

---
+++ components/Card.js
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import React from "react";
+import styled from "styled-components";
+
+const CardElement = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 80px;
+  gap: 16px;
+
+  .active {
+    background: linear-gradient(
+      156.79deg,
+      rgba(255, 248, 241, 0.45) 0.44%,
+      #fff8f2 61.76%,
+      #e0fbe5 134.86%
+    );
+  }
+
+  box-shadow: 25px 25px 100px rgba(166, 161, 157, 0.2);
+  border-radius: 12px;
+`;
+function Card() {
+  return (
+    <CardElement>
+      <Image
+        style={{ backgroundColor: element.bgColor }}
+        src={Element.img}
+        alt={element.altText}
+      />
+      <p>{element.title}</p>
+      <p>{element.details}</p>
+    </CardElement>
+  );
+}
+
+export default Card;
