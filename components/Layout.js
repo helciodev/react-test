76ee0c01dfa3a65105bcb556197d1bb3619d8d63
helciodev <helcioandre@live.com>
2022-06-12 10:19:46 +0100

    Import Header and Footer elements to Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
 
 function Layout({ children }) {
-  return <>{children}</>;
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
 }
 
 export default Layout;
