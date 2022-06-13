666a2969cd884a25d488e058490adba147efcd5a
helciodev <helcioandre@live.com>
2022-06-13 08:34:57 +0100

    Add header and text to choose and convert section

--- sections/ChooseAndCovert.js
+++ sections/ChooseAndCovert.js
@@ -1,9 +1,24 @@
 import React from "react";
 import styled from "styled-components";
+import SectionText from "../components/SectionText";
 
 const ChooseAndConvertSection = styled.section``;
 function ChooseAndCovert() {
-  return <ChooseAndConvertSection></ChooseAndConvertSection>;
+  const heading = `
+    Choose and Convert your Wallet Anytime!
+    `;
+  const text = `A Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
+A Lorem Ipsum is simply`;
+
+  return (
+    <ChooseAndConvertSection>
+      <div className='choose-and-convert-img'></div>
+
+      <div className='choose-and-convert-text-info'>
+        <SectionText heading={heading} text={text} />
+      </div>
+    </ChooseAndConvertSection>
+  );
 }
 
 export default ChooseAndCovert;
