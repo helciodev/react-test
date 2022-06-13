fd6f3f78ae08f0b538efa66e11cb686f3f71dd1f
helciodev <helcioandre@live.com>
2022-06-13 10:41:09 +0100

    Style call to action button on choose and convert section

--- sections/ChooseAndCovert.js
+++ sections/ChooseAndCovert.js
@@ -8,6 +8,7 @@ const ChooseAndConvertSection = styled.section`
   padding: 2rem 3rem;
   display: flex;
   flex-direction: column;
+  align-items: center;
 
   .choose-and-convert-img {
     align-self: flex-start;
@@ -20,6 +21,27 @@ const ChooseAndConvertSection = styled.section`
       z-index: 3;
       height: 100px;
     }
+  }
+  button {
+    height: 56px;
+    width: 327px;
+    border-radius: 5px;
+    padding: 24px, 48px, 24px, 48px;
+    outline: none;
+    border: 1px solid var(--call-to-action);
+    color: var(--call-to-action);
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 24px;
+    letter-spacing: 0.016em;
+    text-align: center;
+    transition: all 0.2s ease-in;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: var(--call-to-action);
+    color: #fff;
   }
 `;
 function ChooseAndCovert() {
@@ -40,6 +62,8 @@ A Lorem Ipsum is simply`;
 
       <div className='choose-and-convert-text-info'>
         <SectionText heading={heading} text={text} />
+
+        <button>Choose your Wallet</button>
       </div>
     </ChooseAndConvertSection>
   );
