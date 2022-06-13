6dc304aeabaf970e748bcf1488e0f2ba4c3c3102
helciodev <helcioandre@live.com>
2022-06-13 10:25:18 +0100

    Change image on choose and convert section

--- sections/ChooseAndCovert.js
+++ sections/ChooseAndCovert.js
@@ -2,20 +2,16 @@ import Image from "next/image";
 import React from "react";
 import styled from "styled-components";
 import SectionText from "../components/SectionText";
-import cryptoAndPc from "../assets/crypto-and-pc.png";
-import pattern from "../assets/pattern-background.png";
+import cryptoAndPc from "../assets/computer-and-crypto.jpeg";
 
 const ChooseAndConvertSection = styled.section`
   padding: 2rem 3rem;
   display: flex;
   flex-direction: column;
-  align-items: center;
 
   .choose-and-convert-img {
-    position: relative;
-    background-image: url(${pattern});
-    background-repeat: no-repeat;
-    background-color: green;
+    align-self: flex-start;
+    margin-bottom: 7rem;
     border-top-right-radius: 100px;
     border-bottom-right-radius: 100px;
     height: 206px;
@@ -23,22 +19,6 @@ const ChooseAndConvertSection = styled.section`
     img {
       z-index: 3;
       height: 100px;
-    }
-    .glass {
-      position: absolute;
-      z-index: 2;
-    }
-
-    .glass {
-      width: 300px;
-      height: 300px;
-      border-radius: 150px;
-      backdrop-filter: blur(18px) saturate(180%);
-      -webkit-backdrop-filter: blur(18px) saturate(180%);
-      background-color: rgba(213, 216, 222, 0.36);
-      border: 1px solid rgba(255, 255, 255, 0.125);
-      top: -28px;
-      right: 310px;
     }
   }
 `;
@@ -52,8 +32,6 @@ A Lorem Ipsum is simply`;
   return (
     <ChooseAndConvertSection>
       <div className='choose-and-convert-img'>
-        <div className='glass'></div>
-
         <Image
           src={cryptoAndPc}
           alt='a computer illustrating making money online'
