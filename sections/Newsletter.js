20d175e5ddb2f09691dbe98fc44362c66492bc8b
helciodev <helcioandre@live.com>
2022-06-13 11:46:59 +0100

    Create newsletter section

---
+++ sections/Newsletter.js
@@ -0,0 +1,7 @@
+import React from "react";
+const NewsLetterSection = styled.section``;
+function Newsletter() {
+  return <NewsLetterSection>Newsletter</NewsLetterSection>;
+}
+
+export default Newsletter;
